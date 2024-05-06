1197daa0b6e338240a99b2a5eebc94b3a7b0294d
Srinivasan R <92676627+Srinivasan8888@users.noreply.github.com>
2024-05-06 13:11:50 +0530

    fixed the filter-bar for mobile view

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="bar">
+      <div className="filter-bar">
         <div className="filter-item">
           <Multiselect
             options={roleOptions}
