22fc6d20c848a4c530dc3a6b5426c0299876d3c0
Srinivasan R <92676627+Srinivasan8888@users.noreply.github.com>
2024-05-06 12:59:34 +0530

    added responsive for the mobile view

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="filter-bar">
+      <div className="bar">
         <div className="filter-item">
           <Multiselect
             options={roleOptions}
